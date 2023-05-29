cd5c6ffa9bd93bb3c725d59258a6c233f48a065d
trtonmoy <tr.tonmoy0110.trt@gmal.com>
2023-05-30 01:42:04 +0600

    data has been loaded from server

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const products = require("./data/data.json");
+
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -10,6 +12,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("warrior kids server is running");
+});
+
+app.get("/products", (req, res) => {
+  res.send(products);
+});
+
+app.get("/products/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedId = products.find((product) => parseInt(product.id) === id);
+  res.send(selectedId);
 });
 
 app.listen(port, () => {
